80cdbde25e415aa462a164d5357e5aa311481267
Cristian Villalobos <cr.villalobos87@gmail.com>
2021-11-25 11:34:59 +0100

    Update model/arima.py predict function

--- water_pollution/model/arima.py
+++ water_pollution/model/arima.py
@@ -6,30 +6,32 @@ from statsmodels.tsa.arima_model import ARIMA
 def predict(df, length):
     '''
     Input:
-    - time serie                          (pd.serie)
-    - forecast length in months           (int)
+    - time serie                      (pd.serie)
+    - forecast length in months       (int)
 
     It performs an ARIMA prediction of the time series for the next 'length' months.
 
     Returns:
-    - deseasonalized and linearized serie (pd serie)
-    - forecast serie                      (pd serie)
-    - std_err                             (np.ndarray)
-    - confidence interval serie           (np.ndarray)
-    - forecast length in months           (np.ndarray)
+    - forecast serie                  (pd serie)
+    - lower confidence interval serie (np.ndarray)
+    - upper confidence interval serie (np.ndarray)
     '''
+    # Multiplicative seasonal decomposition
     result_mul = seasonal_decompose(df['1340'], model = 'multiplicative', period = 12)
-
     df['deseasonalized'] = df['1340'].values/result_mul.seasonal
+
+    # Log-linearization
     df['linearized'] = np.log(df['deseasonalized'])
 
-    train = df['linearized']
+    df = df['linearized']
     p, d, q = 3, 1, 2
 
-    arima = ARIMA(train, order=(p,d,q)).fit()
+    # Model
+    arima = ARIMA(df, order=(p,d,q)).fit()
     forecast, std_err, confidence_int = arima.forecast(length, alpha=0.05)
+
+    # Exponentialization and reseasonalization
     forecast = np.exp(forecast)*result_mul.seasonal[-length:]
-    train = np.exp(train)*result_mul.seasonal
     lower = np.exp(confidence_int)[:,0]*result_mul.seasonal[-length:]
     upper = np.exp(confidence_int)[:,1]*result_mul.seasonal[-length:]
-    return train, forecast, std_err, confidence_int, length
+    return forecast, lower, upper
