d688b8dcc20111a08784454b004f58d2433d5d35
Sebastien VEY <sebvey@gmail.com>
2021-12-02 10:07:06 +0100

    API now caches the requests, if prediction already done, simply returns the cached data

---
+++ notebooks/sebvey/api_data/README.md
@@ -0,0 +1,11 @@
+Modèle entrainé sur toutes les données de la saone entre 2011 et 2021
+sauf la station 60055000 gardée pour la validation
+
+traindf : le df des données d'entrainement
+valdf : le df des données de validation
+
+scaler.save - sklearn standard scaler a utiliser pour scaler avant préd
+répertoire model : le modèle ANN tensorflow
+
+RMSE du modèle : 2.35
+(MSE de 5.5 ~)
--- water_pollution/api/ann_api.py
+++ water_pollution/api/ann_api.py
@@ -1,12 +1,15 @@
 from water_pollution.api import ann_utils as util
 
 from fastapi import FastAPI
-
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+from datetime import date, timedelta
+import os
 import pandas as pd
+from dotenv import load_dotenv
 
-
-from tensorflow.keras import models
-
+load_dotenv()  # Loads env variable from the root .env file
+SAV_PATH = os.getenv('API_SAV_DIR')
 
 
 app = FastAPI()
@@ -14,8 +17,6 @@ app = FastAPI()
 @app.get("/")
 def index():
     return {"ok": True}
-
-
 
 @app.get("/predict")
 def predict(station_id):
@@ -25,10 +26,23 @@ def predict(station_id):
     except:
         return { 'error':'bad format for station_id '}
 
-    predicteddf = util.get_station_weather_prediction_df(station_id)
+    # Build the cache filename
+    stamp = date.today().strftime('%Y%m%d')
+    cachefile_name = stamp + str(station_id) + '.cache'
+    cachefile_path = SAV_PATH + 'cache/' + cachefile_name
 
-    if predicteddf is None :
-        return { 'error':'station_id unknown'}
+    # If cache file exists, loads from it
+    if os.path.isfile(cachefile_path):
+        predicteddf = pd.read_pickle(cachefile_path)
+    # Else, builds the prediction from WeatherAPI
+    else:
+        predicteddf = util.get_station_weather_prediction_df(station_id)
+        if predicteddf is None :
+            return { 'error':'station_id unknown'}
+
+        predicteddf.to_pickle(cachefile_path)
+
+
 
     # FORMAT THE DF TO JSON HERE
 
